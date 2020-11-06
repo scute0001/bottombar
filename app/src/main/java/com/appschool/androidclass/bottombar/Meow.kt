0a50fcef373e6dfbcf7394d393723018e2d82701
Scute0001 <s12t8811@gmail.com>
2020-11-06 11:31:51 +0800

    first cowork branch

--- app/src/main/java/com/appschool/androidclass/bottombar/Meow.kt
+++ app/src/main/java/com/appschool/androidclass/bottombar/Meow.kt
@@ -3,9 +3,12 @@ package com.appschool.androidclass.bottombar
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 
 class Meow: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+
+        Log.i("test", "test")
 
 
 
